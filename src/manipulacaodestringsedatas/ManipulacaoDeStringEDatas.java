6d9cf6679faa746e631f399f835ba8f05056a823
Jardison <jardison.santos1894@gmail.com>
2022-09-04 21:14:12 -0300

    commit de manipulação de dados

--- src/manipulacaodestringsedatas/ManipulacaoDeStringEDatas.java
+++ src/manipulacaodestringsedatas/ManipulacaoDeStringEDatas.java
@@ -1,7 +1,15 @@
-package manipulicaodestringsedatas;
+package manipulacaodestringsedatas;
 
 public class ManipulacaoDeStringEDatas {
 	public static void main(String[] args) {
+		//manipulação de strings
+		String nome = "Jardison";
+		System.out.println(nome.toUpperCase());
+		System.out.println(nome.toLowerCase());
+		System.out.println(nome.length());
 		
+		String nomeOutro="jardison";
+		System.out.println(nome.equals(nomeOutro));
+		System.out.println(nome.equalsIgnoreCase(nomeOutro));
 	}
 }
